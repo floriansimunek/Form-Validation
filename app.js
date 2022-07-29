0c1bef676ae4e5a8983ea91f55d05ddd843e6281
Florian SIMUNEK <florian.simunek@gmail.com>
2022-07-29 19:35:53 +0200

    :sparkles: Get Input + Prevent form submission

---
+++ app.js
@@ -0,0 +1,17 @@
+// form
+const form = document.querySelector("#form");
+
+// email
+const email = document.querySelector("#email");
+const emailConfirm = document.querySelector("#emailConfirm");
+
+// password
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#passwordConfirm");
+
+// submit
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+});
